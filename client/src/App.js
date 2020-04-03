733d2ec115de2d37f14e5f3c0a2cd1b048ebb5af
Von'Erik Barren-Tucker <vonerikbarren-tucker@VonEriks-MacBook-Pro.local>
2020-04-03 17:01:02 -0400

    creation of scatter plot and color updates

--- client/src/App.js
+++ client/src/App.js
@@ -1,13 +1,18 @@
 import React from 'react';
 import './App.css';
 
-import D3App from './components/D3/D3App';
+import D3App from './components/D31/D3App';
+import D32App from './components/D32/D32App';
+import D33App from './components/D33/D33App';
 
 function App() {
   return (
     <div className="App">
       <h1>Gender General Data Visualization</h1>
       <D3App />
+      <D33App />
+
+
     </div>
   );
 }
