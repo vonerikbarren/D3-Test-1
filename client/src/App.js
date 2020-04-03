126624a8be463c6c75771b27a6722aa42d15ac05
Von'Erik Barren-Tucker <vonerikbarren-tucker@VonEriks-MacBook-Pro.local>
2020-04-03 09:54:12 -0400

    D3 Chart update with Man vs women data update

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,13 @@
 import React from 'react';
-
 import './App.css';
+
+import D3App from './components/D3/D3App';
 
 function App() {
   return (
     <div className="App">
-      <h1>React App Ready!</h1>
+      <h1>Gender General Data Visualization</h1>
+      <D3App />
     </div>
   );
 }
